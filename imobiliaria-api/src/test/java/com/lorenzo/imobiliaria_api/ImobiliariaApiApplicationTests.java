c075379679b6b6446b3de8ca5b6a1b95d5a061ed
lorenzo04andreoli <lorenzo04andreoli@gmail.com>
2026-08-31 22:02:57 -0300

    initial commit

---
+++ imobiliaria-api/src/test/java/com/lorenzo/imobiliaria_api/ImobiliariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorenzo.imobiliaria_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImobiliariaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
